7182b471a60e91259b4ef6ad0c22ad1648cfa1b4
Madisson Rivas <msermenor@miug.edu.gt>
2024-03-09 13:02:20 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Serie_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("¡Bienvenido usuario!");
+            Console.WriteLine("Ingrese su correo electronico");
+            Console.ReadLine();
+
+            if (gmail == @gmailcom);
+            {
+                Console.WriteLine(gmail.com + @miumg.edu.gt);
+            }
+
+        }
+    }
+}
